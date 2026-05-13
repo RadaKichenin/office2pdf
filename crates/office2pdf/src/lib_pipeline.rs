dff618fb73dfc1d8fdf2d8be67c8544048e02894
Vlad-Sebastian Cretu <vlad.sebastian.cretu.dev@gmail.com>
2026-05-13 21:15:48 +0300

    wasm Instant
    Signed-off-by: Vlad-Sebastian Cretu <vlad.sebastian.cretu.dev@gmail.com>

--- crates/office2pdf/src/lib_pipeline.rs
+++ crates/office2pdf/src/lib_pipeline.rs
@@ -1,5 +1,8 @@
 use std::collections::HashSet;
+#[cfg(not(target_arch = "wasm32"))]
 use std::time::Instant;
+#[cfg(target_arch = "wasm32")]
+use web_time::Instant;
 
 use crate::config::{ConvertOptions, Format};
 use crate::error::{ConvertError, ConvertMetrics, ConvertResult, ConvertWarning};
